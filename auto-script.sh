7b58ffd2dc178d22fffaffebbc9fa5058491c576
bullblock <steven.liang@icloud.com>
2022-08-15 17:11:47 +0800

    updated

---
+++ auto-script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+cd /home/devops/workspaces/tz-vlp-labs
+git pull
+cd /home/devops
+printf "%s" "Please wait, the system will be get ready in 10 mins ..."
+while ! (nc -z -v -w5 vc.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 avi.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 172.16.0.194 6443 &> /dev/null)
+do
+  printf "%c" "."
+  sleep 5
+done
+printf "\n%s\n" "Okey, here you go !!!"
+sleep 30
+exit
+
