84e1b56a6be2e91d90b2c8f5dc3bf4d860589bd6
bullblock <steven.liang@icloud.com>
2022-08-19 01:37:32 +0800

    updated

--- auto-script-wcp.sh
+++ auto-script-wcp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-cd /home/devops/workspaces/tz-vlp-labs
-git pull
+cd /home/devops/workspaces/tz-vlp-labs &> /dev/bull
+git pull &> /dev/null
 /home/devops/workspaces/tz-vlp-labs/auto-script-wcp-run.sh
