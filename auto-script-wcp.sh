c096098ccbc763235dac9343e945da2d08da633c
bullblock <steven.liang@icloud.com>
2022-08-19 01:10:45 +0800

    updated

--- auto-script-wcp.sh
+++ auto-script-wcp.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 printf "%s" "Please wait, the system will be get ready in 10 mins ..."
+sleep 180
 while ! (nc -z -v -w5 vc.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 avi.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 172.16.0.225 6443 &> /dev/null)
 do
   printf "%c" "."
@@ -7,6 +8,9 @@ do
 done
 echo ""
 printf "%s" "few more mins, you may go to grab a cup of coffee now and come back later ..."
+export KUBECTL_VSPHERE_PASSWORD="Admin!23Admin"
+kubectl vsphere login --server=https://wcp.vlp.tanzulab.io -u administrator@vsphere.local 
+kubectl config use-context wcp.vlp.tanzulab.io
 while (kubectl get pod -A | grep -i csi | grep -v Running &> /dev/null)
 do
   printf "%c" "."
