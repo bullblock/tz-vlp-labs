f41dd7471ec3f1518cc165013d230dcbf4da92f4
bullblock <steven.liang@icloud.com>
2022-08-20 00:43:55 +0800

    updated

--- auto-script-wcp-run.sh
+++ auto-script-wcp-run.sh
@@ -20,18 +20,18 @@ do
     sleep 5
   done
 done
-kubectl vsphere login --server=https://wcp.vlp.tanzulab.io -u administrator@vsphere.local --tanzu-kubernetes-cluster-namesapce=tanzulab --tanzu-kubernetes-cluster-name=vlp-cls-1 &> /dev/null
-kubectl config use-context vlp-cls-1 &> /dev/null
-kubectl delete -f /home/devops/workspaces/tz-vlp-labs/microservices-demo/deploy/kubernetes/complete-demo.yaml &> /dev/null
-kubectl apply -f /home/devops/workspaces/tz-vlp-labs/wcp/psp.yaml &> /dev/null
-kubectl apply -f /home/devops/workspaces/tz-vlp-labs/microservices-demo/deploy/kubernetes/complete-demo.yaml &> /dev/null
-echo ""
-printf "%s" "ok, ok, ok almost there ..."
-while ! (nc -z -v -w5 172.16.0.230 80 &> /dev/null)
-do
-  printf "%c" "."
-  sleep 5
-done
+# kubectl vsphere login --server=https://wcp.vlp.tanzulab.io -u administrator@vsphere.local --tanzu-kubernetes-cluster-namesapce=tanzulab --tanzu-kubernetes-cluster-name=vlp-cls-1 &> /dev/null
+# kubectl config use-context vlp-cls-1 &> /dev/null
+# kubectl delete -f /home/devops/workspaces/tz-vlp-labs/microservices-demo/deploy/kubernetes/complete-demo.yaml &> /dev/null
+# kubectl apply -f /home/devops/workspaces/tz-vlp-labs/wcp/psp.yaml &> /dev/null
+# kubectl apply -f /home/devops/workspaces/tz-vlp-labs/microservices-demo/deploy/kubernetes/complete-demo.yaml &> /dev/null
+# echo ""
+# printf "%s" "ok, ok, ok almost there ..."
+# while ! (nc -z -v -w5 172.16.0.230 80 &> /dev/null)
+# do
+#   printf "%c" "."
+#   sleep 5
+# done
 printf "\n%s\n" "Okey, here you go !!! details please click the README on desktop"
 sleep 30
 exit
