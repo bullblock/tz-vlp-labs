f12175f1932f22681c08b6b50fa1fd26c2ea03e7
bullblock <steven.liang@icloud.com>
2022-08-20 12:31:51 +0800

    updated

---
+++ auto-script-tkg-run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+echo "You may click the README on the desktop for details."
+printf "%s" "Please wait, the system will be get ready in 10 mins ..."
+sleep 60
+
+while ! (nc -z -v -w5 vc.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 avi.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 172.16.0.20 6443 &> /dev/null)
+do
+  printf "%c" "."
+  sleep 5
+done
+
+echo ""
+printf "%s" "few more mins, you may go to grab a cup of coffee now and come back later ..."
+kubectl config use-context vlp-cls-1 &> /dev/null
+
+while ! (nc -z -v -w5 172.16.0.131 6443 &> /dev/null)
+do
+  printf "%c" "."
+  sleep 5
+done
+
+while ! (nc -z -v -w5 172.16.0.225 80 &> /dev/null)
+do
+  printf "%c" "."
+  sleep 5
+done
+
+printf "\n%s\n" "Okey, here you go !!! details please click the README on desktop"
+sleep 30
+exit
+
--- auto-script-tkg.sh
+++ auto-script-tkg.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/bash
-printf "%s" "Please wait, the system will be get ready in 10 mins ..."
-while ! (nc -z -v -w5 vc.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 avi.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 172.16.0.131 6443 &> /dev/null)
-do
-  printf "%c" "."
-  sleep 5
-done
-echo ""
-printf "%s" "few more mins, you may go to grab a cup of coffee now and come back later ..."
-while ! (nc -z -v -w5 172.16.0.225 80 &> /dev/null)
-do
-  printf "%c" "."
-  sleep 5
-done
-printf "\n%s\n" "Okey, here you go !!! details please click the README on desktop"
-sleep 30
-exit
-
+cd /home/devops/workspaces/tz-vlp-labs
+git pull --quiet
+/home/devops/workspaces/tz-vlp-labs/auto-script-tkg-run.sh
