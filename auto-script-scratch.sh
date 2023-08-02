00002984332c6cb24d270acba2ea47e5178cbd35
Steven Liang <24726520+bullblock@users.noreply.github.com>
2023-08-02 22:16:47 +0800

    Update auto-script-scratch.sh

--- auto-script-scratch.sh
+++ auto-script-scratch.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 sudo apt update && sudo apt upgrade -y
+brew update
+brew upgrade
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip-review
+pip-review -a -C
 cd /home/devops/workspaces/tz-vlp-labs 
 git pull --quiet
