290888f1faea5b11078a61ba7d065fa16e23fae3
bullblock <steven.liang@icloud.com>
2023-10-09 11:10:00 +0800

    updated

--- auto-script-scratch.sh
+++ auto-script-scratch.sh
@@ -7,5 +7,3 @@ python3 -m pip install --upgrade pip-review
 pip-review -a -C 2> /dev/null
 cd /home/devops/workspaces/tz-vlp-labs 
 git pull --quiet
-# sudo docker-compose -f /home/devops/workspaces/tz-vlp-labs/harbor/docker-compose.yml up -d
-# sudo docker-compose -f /home/devops/workspaces/tz-vlp-labs/gitlab/docker-compose.yml up -d
