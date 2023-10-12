39b4c45cae69a99b572d7c92f9d36754ecf7e617
bullblock <steven.liang@icloud.com>
2023-10-12 14:16:07 +0800

    updated

---
+++ auto-script-avi-run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+echo "You may click the README on the desktop for details."
+printf "%s" "Please wait, the system will be get ready in 10 mins ..."
+sleep 60
+while ! (nc -z -v -w5 vc.vlp.tanzulab.io 443 &> /dev/null && nc -z -v -w5 avi.vlp.tanzulab.io 443 &> /dev/null)
+do
+  printf "%c" "."
+  sleep 5
+done
+echo ""
+printf "\n%s\n" "Okey, here you go !!! details please click the README on desktop"
+sleep 30
+exit
+
