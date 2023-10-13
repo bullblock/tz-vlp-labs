6b6e4e1852b1b075cf164aab0747b020cd5f7a37
bullblock <steven.liang@icloud.com>
2023-10-14 00:50:32 +0800

    updated

--- auto-script-scratch.sh
+++ auto-script-scratch.sh
@@ -5,5 +5,7 @@ brew upgrade
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pip-review
 pip-review -a -C 2> /dev/null
+brew update
+brew upgrade
 cd /home/devops/workspaces/tz-vlp-labs 
 git pull --quiet
