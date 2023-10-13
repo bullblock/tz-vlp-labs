19fb58ea19904a7a03701d0ea8aa80d9cd82e90a
bullblock <steven.liang@icloud.com>
2023-10-13 19:44:42 +0800

    updated

--- auto-script-tkg.sh
+++ auto-script-tkg.sh
@@ -5,6 +5,8 @@ brew upgrade
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pip-review
 pip-review -a -C 2> /dev/null
+brew update
+brew upgrade
 cd /home/devops/workspaces/tz-vlp-labs
 git pull --quiet
 /home/devops/workspaces/tz-vlp-labs/auto-script-tkg-run.sh
