b82aababc4874999f73753bbb8f702a580795998
bullblock <steven.liang@icloud.com>
2023-10-14 17:24:25 +0800

    updated

--- auto-script-tkg-run.sh
+++ auto-script-tkg-run.sh
@@ -10,15 +10,6 @@ do
 done
 
 echo ""
-printf "%s" "few more mins, you may go to grab a cup of coffee now and come back later ..."
-kubectl config use-context tap &> /dev/null
-
-while nc -z -v -w5 tap.vlp.tanzulab.io 6443 &> /dev/null
-do
-  printf "%c" "."
-  sleep 5
-done
-
 printf "\n%s\n" "Okey, here you go !!! details please click the README on desktop"
 sleep 30
 exit
